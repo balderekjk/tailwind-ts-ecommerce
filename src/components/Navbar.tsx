48e5d16505d73b1bf032cd247d600fb7769cd65f
Derek Bal <bal.derek.j@gmail.com>
2023-02-20 12:28:33 -1000

    revised a little of the flow

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,18 +9,18 @@ const Navbar = () => {
   return (
     <nav className="h-[64px] border-b border-slate-400 py-2">
       <div className={`flex flex-wrap items-start justify-between`}>
-        <div className="flex items-center gap-2 pt-2">
+        <div className="flex cursor-pointer items-center gap-2 pt-2">
           <img
             src="https://assets.website-files.com/593997d928ae740a8b033d30/593997da28ae740a8b0341e1_LAPIS-LAZULI_ICON.png"
             alt="site logo"
             className="h-8"
           />
-          <p className="text-xl font-bold italic text-blue-900">
+          <p className="text-xl font-bold italic text-blue-900 hover:text-blue-700">
             Gems of Grace
           </p>
         </div>
         <div
-          className="flex cursor-pointer items-start p-2 md:hidden"
+          className="flex cursor-pointer items-start p-2 hover:bg-slate-100 hover:text-blue-600 md:hidden"
           onClick={() => setIsCollapsed((prev) => !prev)}
         >
           {isCollapsed ? <Menu /> : <X />}
@@ -50,11 +50,11 @@ const Navbar = () => {
             </Dropdown>
           </NavItem>
         </div>
-        <div className="flex w-full flex-col md:flex md:w-fit md:flex-row md:pt-2 [&>*]:pb-5 [&_div]:cursor-pointer">
-          <NavItem isCollapsed={isCollapsed}>
+        <div className="absolute right-10 flex w-fit flex-row pt-2 md:relative md:right-0 [&_div]:cursor-pointer">
+          <NavItem isCollapsed={false}>
             <ShoppingCart />
           </NavItem>
-          <NavItem isCollapsed={isCollapsed}>
+          <NavItem isCollapsed={false}>
             <User />
           </NavItem>
         </div>
